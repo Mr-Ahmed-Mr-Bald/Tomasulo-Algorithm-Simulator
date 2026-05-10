3cfdd36b58359637ee35a873e741996f35c27581
Omar Beheiry <omarbeheiry@aucegypt.edu>
2026-05-10 17:35:19 +0300

    implemented config.cpp

---
+++ src/core/config.cpp
@@ -0,0 +1,13 @@
+#include "config.h"
+
+int get_reservation_station_size(int addr){
+    if(addr < NUM_REGS && addr > 0)
+        return reservation_station_size[addr];
+    return -1;
+}
+
+int get_execution_cycles(int addr){
+    if(addr < NUM_REGS && addr > 0)
+        return execution_cycles[addr];
+    return -1;
+}
