81019d0791623c23ce8dceb14febf61d0bc5339d
Omar Beheiry <omarbeheiry@aucegypt.edu>
2026-05-11 13:54:47 +0300

    added reservation station class (empty) for returning)

--- include/core/enums.h
+++ include/core/enums.h
@@ -2,7 +2,7 @@
 #define ENUMS_H
 
 enum class Opcode { LOAD, STORE, BEQ, CALL, RET, ADD, SUB, AND, MUL };
-enum class RSClass { Load, Store, Branch, CallRet, AddSub, And, Mul };
+enum class RSClass { Empty, Load, Store, Branch, CallRet, AddSub, And, Mul };
 //enum class InputMode { ManualOneByOne, ManualBlock, File };
 enum class StageState { Waiting, Issued, Executing, Finished, Written };
 enum class InputMode { MANUAL_ONE_BY_ONE, MANUAL_BLOCK, FILE };
