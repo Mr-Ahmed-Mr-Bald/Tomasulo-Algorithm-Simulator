666277b32d8d1baba1c7dc833f925c5ddadecaa1
Omar Beheiry <omarbeheiry@aucegypt.edu>
2026-05-11 07:20:43 +0300

    removed redundant

--- include/core/enums.h
+++ include/core/enums.h
@@ -3,7 +3,7 @@
 
 enum class Opcode { LOAD, STORE, BEQ, CALL, RET, ADD, SUB, AND, MUL };
 enum class RSClass { Load, Store, Branch, CallRet, AddSub, And, Mul };
-enum class InputMode { ManualOneByOne, ManualBlock, File };
+//enum class InputMode { ManualOneByOne, ManualBlock, File };
 enum class StageState { Waiting, Issued, Executing, Finished, Written };
 enum class InputMode { MANUAL_ONE_BY_ONE, MANUAL_BLOCK, FILE };
 
